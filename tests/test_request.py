69e99fb3390a8d61f262c67fd29c537122ea86be
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-11-09 23:07:43 +0300

    test

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,17 +1,26 @@
 from entry import app
-from api.api import api_router
 from fastapi.testclient import TestClient
-from schemas.car_schemas import CarOnlyID, FullCar
 
 
-client = TestClient(app)
+def test_1():
+    with TestClient(app=app) as client:
+        id: int = 1
+        color: str = "brown"
+        model: str = "BMW"
+        gsb: str = "auto"
+        json_data = {"id": id, "color": color, "model": model, "gsb": gsb}
+        response = client.post(
+            'http://127.0.0.1:8080/station/new_car1?',
+            headers={"accept": "application/json"},
+            json=json_data)
+        assert response.status_code == 200
 
 
-def test_add_new_car_1():
-    id: int = 1
-    color: str = 'brown'
-    model: str = 'BMW'
-    gsb: str = 'auto'
-    json_data = {'id': id, 'color': color, 'model': model, 'gsb': gsb}
-    response = client.post('/station/new_car1/', json=json_data) #, response_model=CarOnlyID
-    assert response.status_code == 200
+# def test_add_new_car_1():
+#     id: int = 1
+#     color: str = 'brown'
+#     model: str = 'BMW'
+#     gsb: str = 'auto'
+#     json_data = {'id': id, 'color': color, 'model': model, 'gsb': gsb}
+#     response = client.post('/station/new_car1', json=json_data) #, response_model=CarOnlyID
+#     assert response.status_code == 200
